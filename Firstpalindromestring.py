35826474f89d4fde81b1286781b40f32fbe78c43
Giflin <114787368+Giflin@users.noreply.github.com>
2024-02-13 20:37:48 +0530

    Add files via upload

---
+++ Firstpalindromestring.py
@@ -0,0 +1,10 @@
+# Given an array of strings words, return the first palindromic string in the array.
+# If there is no such string, return an empty string "".
+
+# A string is palindromic if it reads the same forward and backward.
+class Solution(object):
+    def firstPalindrome(self, words):
+        for word in words:
+            if word == word[::-1]:
+                return word
+        return ""
